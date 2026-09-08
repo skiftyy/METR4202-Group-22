8b444b6378da37de8bd975630c6ee478bc28b3b7
Georgia Feros <georgia.feros@gmail.com>
2026-09-08 12:41:27 +1000

    changed file layout

---
+++ tb3_exploration/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'tb3_exploration'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='georgia',
+    maintainer_email='georgia@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+		'waypoint_cycler = tb3_exploration.waypoint_cycler:main',
+        ],
+    },
+)
+
